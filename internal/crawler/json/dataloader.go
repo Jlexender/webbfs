cde218db0ab7b030751c14ee4c8edaa0e825a260
Jlexender <aleksandrdmitryevich@gmail.com>
2024-11-01 04:27:59 +0300

    fix

---
+++ internal/crawler/json/dataloader.go
@@ -0,0 +1,25 @@
+package json
+
+import (
+	"encoding/json"
+	"os"
+	"webbfs/internal/graph"
+)
+
+func LoadSeeds(path string) ([]*graph.WebNode, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var seeds []*graph.WebNode
+	err = json.Unmarshal(data, &seeds)
+	if err != nil {
+		return nil, err
+	}
+	for _, seed := range seeds {
+		seed.Children = make([]*graph.WebNode, 0)
+		seed.Edges = make(map[string]int)
+	}
+
+	return seeds, nil
+}
